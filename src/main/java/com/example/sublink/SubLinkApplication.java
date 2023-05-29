8cc09d231119d9d63167f978b97ae1d177afd29c
‘kewei1’ <keweii@qq.com>
2023-05-29 15:42:21 +0800

    1

---
+++ src/main/java/com/example/sublink/SubLinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.sublink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubLinkApplication.class, args);
+    }
+
+}
